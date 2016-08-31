c9b1bf05da346922f3a3e6bb4989d2049a5d7c93
Miguel Mexicano Herrera <miguelmexicano18@gmail.com>
2016-08-31 16:15:34 -0500

    Agregando nuevo Proyecto

---
+++ Imagenes-Desarrollo/imagenes.php
@@ -0,0 +1,39 @@
+<html>
+<head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Imagenes Distintos tamaños</title>
+    <!-- Bootstrap -->
+    <link href="../css/bootstrap.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="../css/miestilo.css">
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+        <div class="col-md-2">
+        </div>
+        <div class="col-md-8 well">
+        	<br>
+        	<legend align="center">Subir Imagen</legend>
+              <label>Seleccione una imagen:</label>
+              <input type="file" name="imagen" class="file">
+              <br>
+              <br>
+              <input type="submit" name="boton_procesa" class="btn btn-block btn-info" value="Procesar">
+
+        </div>
+        <div class="col-md-2">
+        </div>
+
+		</div>
+	</div>
+
+
+
+
+
+
+</body>
+</html>
